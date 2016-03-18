4ad316f190875cfdaf5ba505fb65133df493c8e5
IrineAir <irineair@gmail.com>
2016-03-18 23:26:37 +0200

    add 1803

--- task 4/scriptCalcJQ.js
+++ task 4/scriptCalcJQ.js
@@ -1,99 +1,105 @@
-window.onload = function () {};
-    
-    var res = null;
-    var lastOper = "";
-    var clearField = false;
+window.onload = function () { }
 
-    var calcField = $("#field");
-    calcField[0].value = "";
-    
-    
-        $("button").click(function () {
-            var btn = this;
-            var valBtn = btn.value;
-            switch (valBtn) {
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "0":
-                    if (clearField) {
-                        calcField[0].value = "";
-                        clearField = false;
-                    }
-                    calcField[0].value += valBtn;
-                    break;
-                case ".":
-                    if (clearField) {
-                        calcField[0].value = "";
-                        clearField = false;
-                    }
-                    calcField[0].value += valBtn;
-                    break;
-                case "plusMinus":
-                    calcField[0].value = parseFloat(calcField[0].value * -1);    
-                    break; 
-                case "addition":
-                    clearField = true; 
-                    Calc(res, parseFloat(calcField[0].value), lastOper);
-                    lastOper = valBtn;                
-                    break;
-                case "subtract":
-                    clearField = true;
-                    Calc(res, parseFloat(calcField[0].value), lastOper);
-                    lastOper = valBtn;                
-                    break;
-                case "multiply":
-                    clearField = true;
-                    Calc(res, parseFloat(calcField[0].value), lastOper);
-                    lastOper = valBtn;                
-                    break;
-                case "division":
-                    clearField = true;
-                    Calc(res, parseFloat(calcField[0].value), lastOper);
-                    lastOper = valBtn;                
-                    break;
-                case "getResult":
-                    if (res != null) {
-                        clearField = true;
-                        Calc(res, parseFloat(calcField[0].value), lastOper);
-                        calcField[0].value = res;
-                        res = null;
-                    }
-                    break;
-                case "reset":
-                    res = null;
-                    lastOper = "";
-                    calcField[0].value = "";
-                    break;
+var res = null;
+var lastOper = "";
+var clearField = false;
+
+var calcField = "";
+
+
+$("button").click(function () {
+    var btn = $(this).val();
+    switch (btn) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            if (clearField) {
+                $("#field").val(btn);
+                clearField = false;
+                calcField = "";
             }
-        });
- 
+            calcField += btn;
+            $("#field").val(calcField);
+            break;
 
-    function Calc(arg1, arg2, oper) { 
-        switch(oper) { 
-            case "addition":
-                res = arg1 + arg2;
-                break;
-            case "subtract":
-                res = arg1 - arg2;
-                break;
-            case "multiply":
-                res = arg1 * arg2;
-                break;
-            case "division":
-                res = arg1 / arg2;
-                if (calcField[0].value == 0) { alert("! Запрет / на ноль !"); }
-                break;
-            default:
-                res = arg2;    
-        }
+        case ".":
+            if (clearField) {
+                $("#field").val(btn);
+                calcField = "";
+                clearField = false;
+            }
+            calcField += btn;
+            $("#field").val(calcField);
+            break;
+
+        case "plusMinus":
+            var plusMinus = parseFloat($("#field").val() * -1);
+            $("#field").val(plusMinus);
+            break;
+            
+        case "addition":
+            clearField = true;            
+            Calc(res, parseFloat(calcField), lastOper);
+            lastOper = btn;
+            break;
+        case "subtract":
+            clearField = true;
+            Calc(res, parseFloat(calcField), lastOper);
+            lastOper = btn;
+            break;
+        case "multiply":
+            clearField = true;
+            Calc(res, parseFloat(calcField), lastOper);
+            lastOper = btn;
+            break;
+        case "division":
+            clearField = true;
+            Calc(res, parseFloat(calcField), lastOper);
+            lastOper = btn;
+            break;
+        case "getResult":
+            if (res != null) {
+                clearField = true;
+                Calc(res, parseFloat(calcField), lastOper);
+                $("#field").val(res);
+                calcField = res;
+                res = null;
+            }
+            break;
+        case "reset":
+            res = null;
+            lastOper = "";
+            $("#field").val("");
+            calcField = "";
+            break;
     }
-    
+});
 
-   
+function Calc(arg1, arg2, oper) {
+    switch (oper) {
+        case "addition":
+            res = arg1 + arg2;
+            break;
+        case "subtract":
+            res = arg1 - arg2;
+            break;
+        case "multiply":
+            res = arg1 * arg2;
+            break;
+        case "division":
+            res = arg1 / arg2;
+            if (calcField == 0) { alert("! Запрет / на ноль !"); }
+            break;
+        default:
+            res = arg2;
+    }
+}
+
+
